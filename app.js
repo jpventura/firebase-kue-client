7818b21942e42f6b55c433d48eb8fd1e53a55ba7
JP Ventura <contact@jpventura.com>
2017-12-07 11:24:21 -0200

    :sparkles: Firebase Realtime Database client for Kue

---
+++ app.js
@@ -0,0 +1,17 @@
+class Client {
+
+    static createClientFactory() {
+        throw new Error('Unsupported operation');
+    }
+
+    create() {
+        throw new Error('Unsupported operation');
+    }
+
+    process() {
+        throw new Error('Unsupported operation');
+    }
+
+}
+
+module.exports = Client;
